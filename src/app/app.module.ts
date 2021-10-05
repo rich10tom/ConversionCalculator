a9630a08f691a594708c9e2ffc4a97315d3bf6d8
Richard Thompson <rich10tom@gmail.com>
2021-10-04 19:00:46 -0500

    Starting point

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TemperatureComponent } from './temperature/temperature.component';
+import { SpeedComponent } from './speed/speed.component';
+import { LengthComponent } from './length/length.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TemperatureComponent,
+    SpeedComponent,
+    LengthComponent
   ],
   imports: [
     BrowserModule
