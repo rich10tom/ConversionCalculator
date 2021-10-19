4f907f1877f3192c296c712722a283bfb02a2f16
Richard <rich10tom@gmail.com>
2021-10-18 19:51:02 -0500

    Finished the length and speed calculators.

--- src/app/length/length.component.ts
+++ src/app/length/length.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LengthComponent implements OnInit {
 
+  selector!: string;
+  result!: number;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  convertLength(inputValue: HTMLInputElement) {
+
+    switch (this.selector) {
+      case 'INCM':
+        this.result = +inputValue.value / 0.3937;
+        break;
+      case 'MKM':
+        this.result = +inputValue.value * 1.609344;
+        break;
+      case 'MMPM':
+        this.result = +inputValue.value * 1000000000;
+        break;
+      case 'FTM':
+        this.result = +inputValue.value / 5280 ;
+        break;
+      case 'YFT':
+        this.result = +inputValue.value * 3;
+        break;
+      case 'MY':
+        this.result =  +inputValue.value * 1.093613;
+        break;
+
+      default:
+        alert('Invalid Selection');
+        break;
+    }
+  }
 }
--- src/app/speed/speed.component.ts
+++ src/app/speed/speed.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpeedComponent implements OnInit {
 
+  selector!: string;
+  result!: number;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  convertSpeed(inputValue: HTMLInputElement) {
+
+    switch (this.selector) {
+      case 'MPHKMH':
+        this.result = +inputValue.value * 1.609344;
+        break;
+      case 'KMHMPH':
+        this.result = +inputValue.value/1.609344;
+        break;
+      case 'MMPH':
+        this.result = +inputValue.value * 2.23694;
+        break;
+      case 'FTM':
+        this.result = +inputValue.value * 0.3048;
+        break;
+      case 'MFT':
+        this.result = +inputValue.value / 0.3048;
+        break;
+      case 'MPHK':
+        this.result =  +inputValue.value * 0.868976;
+        break;
+
+      default:
+        alert('Invalid Selection');
+        break;
+    }
   }
 
 }
